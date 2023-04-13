75f2134b663f96a534aa5cb60611aa0895cc54cb
Ransika <57299043+RansikaP@users.noreply.github.com>
2023-04-12 22:39:34 -0400

    another one

--- server.js
+++ server.js
@@ -3,12 +3,11 @@ const app = express();
 const path = require('path');
 
 app.use(express.static(path.join(__dirname, 'build')));
-app.use(express.static("build"));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-const port = process.env.PORT || 5000;
-app.listen(port);
+const PORT = process.env.PORT || 3001;
+app.listen(PORT);
 
-console.log(`App is listening on port ${port}`);
+console.log(`App is listening on port ${PORT}`);
