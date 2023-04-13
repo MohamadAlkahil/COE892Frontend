af23c3484cce4af25e1286a84a8d86b811736a60
Ransika <57299043+RansikaP@users.noreply.github.com>
2023-04-12 22:31:25 -0400

    millionth try is the charm

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const path = require('path');
 
 app.use(express.static(path.join(__dirname, 'build')));
-
+app.use(express.static("build"));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
